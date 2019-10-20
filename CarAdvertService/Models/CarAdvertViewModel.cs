8de39b97890d9152cd7872e7fac7ad5e74e79576
Dawid Nowak <dawid.nowak@yahoo.de>
2019-10-20 15:07:54 +0200

    - Attributes for CarAdvertVM properties defined

--- CarAdvertService/Models/CarAdvertViewModel.cs
+++ CarAdvertService/Models/CarAdvertViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 
 namespace CarAdvertService.Controllers
@@ -18,20 +20,26 @@ namespace CarAdvertService.Controllers
             Mileage = New ? 0 : 100000 - (Id * 900);
             FirstRegistration = New ? DateTime.MinValue : DateTime.Today.AddYears(-Id);
         }
-
+        [Required]
         [DataMember]
         public int Id { get; set; }
+        [Required]
         [DataMember]
         public string Title { get; set; }
+        [Required]
         [DataMember]
+        [Range(0, 2)]
         public int Fuel { get; set; }
+        [Required]
         [DataMember]
         public int Price { get; set; }
+        [Required]
         [DataMember]
         public bool New { get; set; }
         [DataMember]
         public int? Mileage { get; set; }
         [DataMember]
+        [JsonProperty(PropertyName = "First registration")]
         public DateTime? FirstRegistration { get; set; }
     }
 }
