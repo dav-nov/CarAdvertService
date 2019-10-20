ed3acd471c034aa44a0c7be4940926f3efc5f533
Dawid Nowak <dawid.nowak@yahoo.de>
2019-10-20 12:32:46 +0200

    - some basic GUI for GET testing

--- CarAdvertService/Controllers/HomeController.cs
+++ CarAdvertService/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +9,40 @@ namespace CarAdvertService.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: advert
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            IEnumerable<CarAdvertViewModel> adverts = null;
 
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:63915/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("caradvert");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<CarAdvertViewModel>>();
+                    readTask.Wait();
+
+                    adverts = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    adverts = Enumerable.Empty<CarAdvertViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(adverts);
+        }
+
+        public ActionResult Create()
+        {
             return View();
         }
     }
